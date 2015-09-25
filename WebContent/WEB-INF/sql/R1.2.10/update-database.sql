d99b6776423301042b7532ad149b6b7510a57b17
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-09-25 12:34:13 -0300

    Corrección para que el reporte de irregularidades esté considerando todos los empleados separados por coma.

--- WebContent/WEB-INF/sql/R1.2.10/update-database.sql
+++ WebContent/WEB-INF/sql/R1.2.10/update-database.sql
@@ -1,3 +1,4 @@
 update toption set cOrder = 9 where ckey='REP_CONFIG';
 #update treportparametertype set cJavaType='STRING' where cid=1;
 
+update treportparametertype set cJavaType='LONG' where cid=1;
