77f9740a737e97e786a68aac6e08de01b08ca9ec
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-09-28 10:21:17 -0300

    Se actualiza la vista bsframework.vUser

--- WebContent/WEB-INF/sql/R1.2.10/update-database.sql
+++ WebContent/WEB-INF/sql/R1.2.10/update-database.sql
@@ -8,3 +8,12 @@ ALTER TABLE tOption ADD COLUMN cIsAdmin BIT NOT NULL DEFAULT FALSE AFTER cEnable
 update toption set cIsAdmin = true where cKey = 'DOMAIN_MGR';
 update toption set cIsAdmin = true where cKey = 'DOMAIN_ATTR_MGR';
 
+DROP VIEW IF EXISTS bsframework.vUser;
+CREATE VIEW bsframework.vUser
+AS
+		SELECT		a.cId, a.cMail, a.cName 
+		FROM 		bsframework.tUser AS a
+	#	LEFT JOIN	tR_UserDomain AS b ON a.cId = b.cUser
+		WHERE		!a.cAdmin;
+ 
+		
