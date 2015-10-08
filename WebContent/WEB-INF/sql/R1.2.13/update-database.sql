1261938de6438aacb9d48d544fc532dbb0e7f9cc
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-10-08 17:28:25 -0300

    Se crea tabla tCrewLog

--- WebContent/WEB-INF/sql/R1.2.13/update-database.sql
+++ WebContent/WEB-INF/sql/R1.2.13/update-database.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS tCrewLog(
+	cId				BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cAttendanceLog	BIGINT NOT NULL
+) ENGINE=innoDB;
+
+ALTER TABLE tCrewLog
+ADD INDEX CrewLog_index_AttendanceLog (cAttendanceLog ASC),
+ADD CONSTRAINT CrewLog_To_AttendanceLog FOREIGN KEY (cAttendanceLog) REFERENCES tAttendanceLog(cId);
 
 UPDATE tVersion SET cVersion='1.2.13', cUpdated=NOW() WHERE cKey = 'DBT';
 
