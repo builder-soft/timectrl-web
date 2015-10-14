d4a8b05c2f123de080c0f11c915402d44e1e597b
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-10-14 11:09:19 -0300

    Se avanza en el proceso que permite el llenado de la tabla tCrewProcess

--- WebContent/WEB-INF/sql/R1.2.13/reset.cmd
+++ WebContent/WEB-INF/sql/R1.2.13/reset.cmd
@@ -1,3 +1,3 @@
-mysql -u root -padmin -e "DROP DATABASE IF EXISTS enlasa;CREATE DATABASE enlasa;"
-mysql -u root -padmin enlasa < D:\google-drive\timecontrol-install\RespaldoDB\test-unit\backup_enlasa_R1.2.10_20151001.txt
-call run-once enlasa
+mysql -u root -padmin -e "DROP DATABASE IF EXISTS rsa;CREATE DATABASE rsa;"
+mysql -u root -padmin rsa < C:\cmoscoso\google-drive\timecontrol-install\RespaldoDB\test-unit\backup_enlasa_R1.2.10_20151001.txt
+call run-once rsa
--- WebContent/WEB-INF/sql/R1.2.13/testSP.sql
+++ WebContent/WEB-INF/sql/R1.2.13/testSP.sql
@@ -1,7 +1,12 @@
 # call pCrew('2015-07-01', '2015-07-30', '10');
 
-select fGetBusinessDay(167, '179', 180, '2015-07-05');
-select fGetBusinessDay(167, '179', 180, '2015-07-06');
-select fGetBusinessDay(167, '179', 180, '2015-07-07');
+#select fGetBusinessDay(167, '179', 180, '2015-07-05');
+#select fGetBusinessDay(167, '179', 180, '2015-07-06');
+#select fGetBusinessDay(167, '179', 180, '2015-07-07');
 
-call pTopAbsence('2015-07-01', '2015-07-10', 200);
+#call pTopAbsence('2015-07-01', '2015-07-10', 200);
+
+
+select fMarkAndUserToTurnDayId4('2014-01-06', 64, 240, true);
+
+
