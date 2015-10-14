6af0673203d54b4f12306b20ee1942d23e1dae54
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-10-14 01:09:45 -0300

    Correcion de reporte de los Top

--- WebContent/WEB-INF/sql/R1.2.13/testSP.sql
+++ WebContent/WEB-INF/sql/R1.2.13/testSP.sql
@@ -1,2 +1,7 @@
-call pCrew('2015-07-01', '2015-07-30', '10');
+# call pCrew('2015-07-01', '2015-07-30', '10');
 
+select fGetBusinessDay(167, '179', 180, '2015-07-05');
+select fGetBusinessDay(167, '179', 180, '2015-07-06');
+select fGetBusinessDay(167, '179', 180, '2015-07-07');
+
+call pTopAbsence('2015-07-01', '2015-07-10', 200);
