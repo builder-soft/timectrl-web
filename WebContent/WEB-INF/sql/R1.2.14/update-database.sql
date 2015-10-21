610c632f6b0977a77d03d24a30b1759ed9217310
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-10-21 00:06:26 -0300

    Actualizacion de vUser

--- WebContent/WEB-INF/sql/R1.2.14/update-database.sql
+++ WebContent/WEB-INF/sql/R1.2.14/update-database.sql
@@ -1,2 +1,10 @@
+DROP VIEW IF EXISTS vUser;
+CREATE VIEW vUser
+AS
+		SELECT		a.cId, a.cMail, a.cName 
+		FROM 		bsframework.tUser AS a
+		LEFT JOIN	bsframework.tR_UserDomain AS b ON a.cId = b.cUser
+		LEFT JOIN	bsframework.tDomain AS c ON b.cDomain = c.cId 
+		WHERE		!a.cAdmin AND c.cDatabase = DATABASE();
 
 UPDATE tVersion SET cVersion='1.2.14', cUpdated=NOW() WHERE cKey = 'DBT';
