ca1b497d28c600abcd7b26e4cc3728983ea7f708
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-11-23 12:56:22 -0300

    Corrección para que el reporte de dotacion considere los días en que nadie marcó.

--- WebContent/WEB-INF/sql/R1.2.19/sp-pCrew.sql
+++ WebContent/WEB-INF/sql/R1.2.19/sp-pCrew.sql
@@ -4,32 +4,57 @@ DELIMITER $$
 
 CREATE PROCEDURE pCrew(IN vStartDate DATE, IN vEndDate DATE, IN vEmployees VARCHAR(2500))
 BEGIN
+	DECLARE vCurrent DATE;
+	
+	SET @vStartDate = vStartDate;
+	SET @vEndDate = vEndDate;
+	
 	DROP TABLE IF EXISTS tCrewProcess_temp;
 	CREATE TABLE IF NOT EXISTS tCrewProcess_temp(
 		cId				BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		cDate			DATE NOT NULL,
-		cEmployee		BIGINT NOT NULL,
+		cEmployee		BIGINT(20) NULL,
 		cHoursWorked	DOUBLE DEFAULT '0',
 		cWorked			BIT	DEFAULT 0,
 		cHired			BIT DEFAULT 0
 	) ENGINE=Memory;
 	
-	INSERT INTO	tCrewProcess_temp(cDate, cEmployee, cHoursWorked, cWorked, cHired)
-	SELECT		cDate, cEmployee, cHoursWorked, cWorked, cHired
-	FROM		tCrewProcess 
-	WHERE		DATE(cDate) BETWEEN vStartDate AND vEndDate
-	ORDER BY	cDate;
+	SET @vSQL = 'INSERT INTO tCrewProcess_temp(cDate, cEmployee, cHoursWorked, cWorked, cHired) ';
+	SET @vSQL = CONCAT(@vSQL, 'SELECT cDate, cEmployee, cHoursWorked, cWorked, cHired ');
+	SET @vSQL = CONCAT(@vSQL, 'FROM tCrewProcess ');
+	SET @vSQL = CONCAT(@vSQL, 'WHERE DATE(cDate) BETWEEN ? AND ? ');
+#	SET @vSQL = CONCAT(@vSQL, 'AND cEmployee IN (', vEmployees,') ');
+	SET @vSQL = CONCAT(@vSQL, ';');
+#	SET @vSQL = CONCAT(@vSQL, 'ORDER BY	cDate;');
+	
+	PREPARE smpt FROM @vSQL;
+	EXECUTE smpt USING @vStartDate, @vEndDate;
+	DEALLOCATE PREPARE smpt;
+	
+#	select @vSQL, ;
+#select * from tCrewProcess_temp;
+
+	SET vCurrent = vStartDate;
+	WHILE vCurrent <= vEndDate DO
+#select  (SELECT COUNT(cId) FROM tCrewProcess_temp WHERE DATE(cDate) = DATE(vCurrent)), vCurrent;
+	
+		IF(SELECT COUNT(cId) FROM tCrewProcess_temp WHERE DATE(cDate) = DATE(vCurrent)=0) THEN
+			INSERT INTO tCrewProcess_temp(cDate, cEmployee, cHoursWorked, cWorked, cHired) VALUES(vCurrent, NULL, 0,FALSE,FALSE);
+#			select 'inserted' ,vCurrent;
+		END IF;
+		SET vCurrent = DATE_ADD(vCurrent, INTERVAL 1 DAY);
+	END WHILE;
 	
 	SET @vSQL = 'SELECT	cDate AS \'Fecha\', ROUND(SUM(cHoursWorked),2) AS \'Horas Trabajadas\', sum(if(cWorked, 1, 0)) AS \'Presente\', sum(if(cHired,1,0)) AS \'Contratados\', IFNULL( ROUND((sum(if(cWorked, 1, 0))*100)/sum(if(cHired,1,0)),2), 0) AS \'% Asistencia\' ';
 #	SET @vSQL = 'SELECT	cDate, SUM(cHoursWorked), COUNT(cWorked), COUNT(cHired), (COUNT(cWorked)*100)/COUNT(cHired) ';
 	SET @vSQL = CONCAT(@vSQL, 'FROM	tCrewProcess_temp AS a ');
-	SET @vSQL = CONCAT(@vSQL, 'WHERE cEmployee IN (', vEmployees,') ');
-	SET @vSQL = CONCAT(@vSQL, 'GROUP BY cDate;');
+	SET @vSQL = CONCAT(@vSQL, 'WHERE cEmployee IN (', vEmployees,') OR cEmployee IS NULL ');
+	SET @vSQL = CONCAT(@vSQL, 'GROUP BY DATE(cDate);');
 
-	SET @vStartDate = vStartDate;
-	SET @vEndDate = vEndDate;
 
-	select @vSQL;
+#select * from tCrewProcess_temp; # GROUP BY cDate;
+#select * from tCrewProcess_temp where date(cdate) = '2015-10-03';
+#select @vSQL;
 	
 	PREPARE smpt FROM @vSQL;
 	EXECUTE smpt; 
