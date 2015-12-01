e6513d8d3a5c1a4e157e6ab91ac4b4869d559751
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-12-01 18:58:46 -0300

    Se mejora el rendimiento del readmarks

--- src/cl/buildersoft/web/servlet/timectrl/machine/ReadAttendance.java
+++ src/cl/buildersoft/web/servlet/timectrl/machine/ReadAttendance.java
@@ -43,6 +43,7 @@ public class ReadAttendance extends BSHttpServlet {
 		} finally {
 			machineService.disconnect(api);
 		}
+		closeConnection(conn);
 
 		request.setAttribute("Attendances", list);
 		request.setAttribute("Machine", machine);
