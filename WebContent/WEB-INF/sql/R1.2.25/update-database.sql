86051aa00a4bd56afcd51b94ea7cc5766cd4f63b
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-03-03 18:43:55 -0300

    Se agregan más eventos al registro de Log

---
+++ WebContent/WEB-INF/sql/R1.2.25/update-database.sql
@@ -0,0 +1,7 @@
+insert into tEventType(cKey, cName) VALUES('EMPLOYEE_INSERT', 'Nuevo empleado');
+insert into tEventType(cKey, cName) VALUES('EMPLOYEE_UPDATE', 'Modificacion datos empleado');
+
+
+UPDATE tVersion SET cVersion='1.2.25', cUpdated=NOW() WHERE cKey = 'DBT';
+
+
