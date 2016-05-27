b77661bdccf6d73b2b9703295009f36f4532c6ab
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-05-27 18:01:28 -0400

    Mejoras para la encriptacion de las marcas.

--- WebContent/WEB-INF/sql/R1.3.1/run-once.cmd
+++ WebContent/WEB-INF/sql/R1.3.1/run-once.cmd
@@ -1,10 +1,10 @@
 @echo off
-echo -- 1.2.27 --
+echo -- 1.3.1 --
 IF "%1" == "" GOTO error
 	for %%i in (fn-*.sql) do mysql -D%1 -t -u root --default-character-set=utf8 < %%i
 
 	for %%i in (sp-*.sql) do mysql -D%1 -t -u root --default-character-set=utf8 < %%i
-
+	echo runing update-database.sql...
 	mysql -D%1 -t -u root --default-character-set=utf8 < update-database.sql
 
 goto fin
--- WebContent/WEB-INF/sql/R1.3.1/update-database.sql
+++ WebContent/WEB-INF/sql/R1.3.1/update-database.sql
@@ -1,15 +1,8 @@
+ALTER TABLE tAttendanceLog CHANGE COLUMN cDate cDate timestamp NULL DEFAULT NULL;
 ALTER TABLE tAttendanceLog ADD COLUMN cHash CHAR(32) COMMENT 'Hash del registro' AFTER cMarkType;
-UPDATE tAttendanceLog SET cHash=md5(concat(cEmployeeKey, "#",cMachine, "#",cDate, "#",cMarkType)); 
+UPDATE tAttendanceLog SET cHash=fGetHash(cEmployeeKey, cMachine, cDate, cMarkType); 
 
 DROP TRIGGER if exists InsertOnAttendanceLog;
 CREATE TRIGGER InsertOnAttendanceLog before INSERT ON tAttendanceLog
 FOR EACH ROW
-BEGIN
-#	set NEW.cHash = concat(NEW.cMachine, "#",NEW.cMarkType);
-	SET NEW.cHash=md5(concat(NEW.cEmployeeKey, "#",NEW.cMachine, "#"NEW.cDate, "#",NEW.cMarkType));
-/* 
-	update tAttendanceLog set cHash = NEW.cId WHERE cId = NEW.cId;
-	INSERT INTO tAttendanceLog(cEnterprise, cTextFootSalary) 
-	VALUES (NEW.cId, '');
-	*/
-END;
+	SET NEW.cHash=fGetHash(NEW.cEmployeeKey, NEW.cMachine, NEW.cDate, NEW.cMarkType);
