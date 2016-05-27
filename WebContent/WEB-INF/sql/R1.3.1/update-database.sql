28e3730a06b54df7f8f5f5d11f828f38d965cea3
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-05-27 00:19:11 -0400

    Actualizacion de trigger para insertar un registro de attendanceLog

--- WebContent/WEB-INF/sql/R1.3.1/update-database.sql
+++ WebContent/WEB-INF/sql/R1.3.1/update-database.sql
@@ -1,14 +1,15 @@
 ALTER TABLE tAttendanceLog ADD COLUMN cHash CHAR(32) COMMENT 'Hash del registro' AFTER cMarkType;
-UPDATE tAttendanceLog SET cHash=md5(concat(cId, "#", cEmployeeKey, "#",cMachine, "#",cDate, "#",cMarkType)); 
+UPDATE tAttendanceLog SET cHash=md5(concat(cEmployeeKey, "#",cMachine, "#",cDate, "#",cMarkType)); 
 
-DROP TRIGER InsertOnAttendanceLog;
-CREATE TRIGGER InsertOnAttendanceLog AFTER INSERT ON tAttendanceLog
+DROP TRIGGER if exists InsertOnAttendanceLog;
+CREATE TRIGGER InsertOnAttendanceLog before INSERT ON tAttendanceLog
 FOR EACH ROW
 BEGIN
-	
+#	set NEW.cHash = concat(NEW.cMachine, "#",NEW.cMarkType);
+	SET NEW.cHash=md5(concat(NEW.cEmployeeKey, "#",NEW.cMachine, "#"NEW.cDate, "#",NEW.cMarkType));
 /* 
+	update tAttendanceLog set cHash = NEW.cId WHERE cId = NEW.cId;
 	INSERT INTO tAttendanceLog(cEnterprise, cTextFootSalary) 
 	VALUES (NEW.cId, '');
 	*/
-END$$
-
+END;
