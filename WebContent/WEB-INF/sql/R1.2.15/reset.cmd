5482190292eeb762dcf45e570dfbe8c484a14c40
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-08-26 14:58:22 -0400

    Homologacion con version que corresponde

--- WebContent/WEB-INF/sql/R1.2.15/reset.cmd
+++
@@ -1,3 +0,0 @@
-mysql -u root -padmin -e "DROP DATABASE IF EXISTS rsa;CREATE DATABASE rsa;"
-mysql -u root -padmin rsa < %GDRIVE_PATH%\timectrl-cloud\backup-database\backup-rsa-20151031.txt
-call run-once rsa
